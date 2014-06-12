191c689af4d40f345360edfd0832bd4549722d8f
Sven Kubiak <sk@svenkubiak.de>
2014-06-12 20:04:56 +0200

    playday overview did fetch only 15 users

--- app/controllers/Overview.java
+++ app/controllers/Overview.java
@@ -21,7 +21,7 @@ public class Overview extends Root{
         final Pagination pagination = ViewUtils.getPagination(number, "/overview/playday/");
 
         final Playday playday = Playday.find("byNumber", pagination.getNumberAsInt()).first();
-        final List<User> users = User.find("SELECT u FROM User u WHERE active = true ORDER BY place ASC").from(0).fetch(15);
+        final List<User> users = User.find("SELECT u FROM User u WHERE active = true ORDER BY place ASC").fetch();
         final List<Map<User, List<GameTip>>> tips = AppUtils.getPlaydayTips(playday, users);
         final long usersCount = User.count();
 
@@ -38,7 +38,7 @@ public class Overview extends Root{
 
     public static void lazy(final int number, final int start) {
         final Playday playday = Playday.find("byNumber", number).first();
-        final List<User> users = User.find("SELECT u FROM User u WHERE active = true ORDER BY place ASC").from(start).fetch(15);
+        final List<User> users = User.find("SELECT u FROM User u WHERE active = true ORDER BY place ASC").fetch();
         final List<Map<User, List<GameTip>>> tips = AppUtils.getPlaydayTips(playday, users);
 
         render(tips);
